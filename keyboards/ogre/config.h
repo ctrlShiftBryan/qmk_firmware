2b3f878ec339bb1e993e9d34023d2df9b79f44c1
Bryan Arendt <barendt@covermymeds.com>
2019-07-16 00:18:48 -0400

    ogre v2 works

--- keyboards/ogre/config.h
+++ keyboards/ogre/config.h
@@ -41,8 +41,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { D2, D1, D0, D4, C6, D7, E6, B4, B5, F4 }
-#define MATRIX_COL_PINS { F5, F6, F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS { D3, D2, D1, D0, D4, C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { F5, F4, F6, F7, B1, B3, B2 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
