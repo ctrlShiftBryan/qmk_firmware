5b30a8b183f34821b3a9ef35b56b96a9e53516c8
Bryan Arendt <barendt@covermymeds.com>
2019-07-17 06:26:59 -0400

    fix top row

--- keyboards/ogre/keymaps/default/keymap.c
+++ keyboards/ogre/keymaps/default/keymap.c
@@ -23,8 +23,8 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_EQL,
-    KC_MINS, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PGUP,
+		KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5, KC_GRV,
+    KC_PGUP, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_RBRC,
     KC_LBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
 		KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_PGDN,
