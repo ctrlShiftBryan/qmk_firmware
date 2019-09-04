8bec65da425575045a5a2f9bf8b645ca1f1f209f
Bryan Arendt <barendt@covermymeds.com>
2019-09-04 09:15:34 -0400

    add bootloader

--- keyboards/ogre/rules.mk
+++ keyboards/ogre/rules.mk
@@ -46,7 +46,7 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 #   LUFA bootloader  4096
 #   USBaspLoader     2048
 OPT_DEFS += -DBOOTLOADER_SIZE=4096
-
+BOOTLOADER = caterina
 
 # Build Options
 #   change yes to no to disable
