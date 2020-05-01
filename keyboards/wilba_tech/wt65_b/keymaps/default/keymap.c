fd21b829d20753c1b8646eefec9991d5bfef9941
Bryan Arendt <barendt@covermymeds.com>
2020-05-01 05:27:43 -0400

    osl

--- keyboards/wilba_tech/wt65_b/keymaps/default/keymap.c
+++ keyboards/wilba_tech/wt65_b/keymaps/default/keymap.c
@@ -16,15 +16,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
 		KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, TP_ENT,                    KC_PGDN,
 		KC_LSFT, TP_Z,    TP_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,   MO(1),
-		TP_GR,          MO(1),                            KC_SPC,                                      KC_LALT,            KC_LEFT, KC_DOWN, KC_RGHT),
+		TP_GR,          OSL(1),                            KC_SPC,                                      KC_LALT,            KC_LEFT, KC_DOWN, KC_RGHT),
 
 	// Fn1 Layer
 	[1] = LAYOUT_all(
 		RESET,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_TRNS,  KC_TRNS,
-		KC_CAPS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_MPRV, KC_PAUS, KC_MNXT, KC_TRNS, KC_BSPC,          KC_TRNS,
+		KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_MPRV, KC_PAUS, KC_MNXT, KC_TRNS, KC_BSPC,          KC_TRNS,
 		KC_TRNS,   KC_VOLD, KC_VOLU, KC_MUTE, KC_EJCT, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,
 		KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS,
-		KC_TRNS,          KC_LALT,                            KC_BSPC,                                     KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS),
+		KC_TRNS,          KC_LGUI,                            KC_BSPC,                                     KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS),
 
 	// Fn2 Layer
 	[2] = LAYOUT_all(
